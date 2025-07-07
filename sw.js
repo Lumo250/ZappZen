f2befbef93676ee00e1bd93d5350ea75f42c6a8f
AG Design <lumo250@libero.it>
2025-07-07 12:37:58 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'ZappZen-cache-v1';
+const OFFLINE_URL = '/';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png',
+        'https://cdn.jsdelivr.net/npm/hls.js@latest'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
